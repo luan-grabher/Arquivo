b5a7e8ae0b0c3c5bd6970dac7828264b54ec3164
Admnistrador <Admnistrador@192.168.0.31>
2021-03-30 17:38:29 -0300

    added method to read text. added param to define separator

--- src/fileManager/CSV.java
+++ src/fileManager/CSV.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class CSV {
 
     /**
-     * Transforma texto de um arquivo CSV em um Mapa. Caso a primeira linah
+     * Transforma texto de um arquivo CSV em um Mapa. Caso a primeira linha
      * comece com #, a primeira linha sera usada como header, caso o contrário,
      * as colunas serão nomeadas com o número da coluna.
      * <p>
@@ -19,54 +19,86 @@ public class CSV {
      * @return Lista de linhas do csv
      */
     public static List<Map<String, String>> getMap(File file) {
+        return getMap(file, ";");
+    }
+
+    /**
+     * Transforma texto de um arquivo CSV em um Mapa. Caso a primeira linha
+     * comece com #, a primeira linha sera usada como header, caso o contrário,
+     * as colunas serão nomeadas com o número da coluna.
+     * <p>
+     * Quebra colunas por ";" Quebra linhas por "\n\r"
+     *
+     * @param colBreaker texto para quebrar as colunas, por padrao use ";"
+     * @param file Arquivo csv
+     * @return Lista de linhas do csv
+     */
+    public static List<Map<String, String>> getMap(File file, String colBreaker) {
         List<Map<String, String>> rows = new ArrayList<>();
 
-        if (Selector.verifyFile(file.getAbsolutePath(), ".csv")) {
+        if (file.exists()) {
             String text = FileManager.getText(file);
-            String[] lines = text.split("\r\n");
+            rows.addAll(getMapFromText(text, colBreaker));
+        }
 
-            //Cria o header
-            Map<Integer, String> headers = new LinkedHashMap<>();
-            String[] firstRowCols = lines[0].replaceFirst("#", "").split(";");
+        return rows;
+    }
 
-            //Se tiver # na primeira linha, irá considerar como as colunas
-            if (lines[0].startsWith("#")) {
-                for (int i = 0; i < firstRowCols.length; i++) {
-                    String col = firstRowCols[i];
-                    //coloca no mapa
-                    headers.put(i, col);
-                }
-            }//Se não irá contar quantas colunas tem na primeira linha e irá definir como as colunas
-            else {
-                for (Integer i = 0; i < firstRowCols.length; i++) {
-                    headers.put(i, i.toString());
-                }
+    /**
+     * Transforma texto em um Mapa. Caso a primeira linha
+     * comece com #, a primeira linha sera usada como header, caso o contrário,
+     * as colunas serão nomeadas com o número da coluna.
+     * <p>
+     * Quebra colunas por ";" Quebra linhas por "\n\r"
+     *
+     * @param colBreaker texto para quebrar as colunas, por padrao use ";"
+     * @param text Arquivo texto
+     * @return Lista de linhas do csv
+     */
+    public static List<Map<String, String>> getMapFromText(String text, String colBreaker) {
+        List<Map<String, String>> rows = new ArrayList<>();
+        String[] lines = text.split("\r\n");
+
+        //Cria o header
+        Map<Integer, String> headers = new LinkedHashMap<>();
+        String[] firstRowCols = lines[0].replaceFirst("#", "").split(colBreaker);
+
+        //Se tiver # na primeira linha, irá considerar como as colunas
+        if (lines[0].startsWith("#")) {
+            for (int i = 0; i < firstRowCols.length; i++) {
+                String col = firstRowCols[i];
+                //coloca no mapa
+                headers.put(i, col);
             }
-
-            //Percorre linhas
-            for (String line : lines) {
-                if (!line.startsWith("#")) {
-                    //Pega colunas da linha
-                    String[] cols = line.split(";");
-
-                    //tryCatch para evitar parar tudo caso alguma linha tenha mais colunas que a primeira
-                    try {
-                        Map<String, String> map = new LinkedHashMap<>();
-
-                        //Para cada header coloca no mapa
-                        headers.forEach((i, name) -> {
-                            map.put(name, cols[i]);
-                        });
-
-                        //coloca mapa na lista
-                        rows.add(map);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+        }//Se não irá contar quantas colunas tem na primeira linha e irá definir como as colunas
+        else {
+            for (Integer i = 0; i < firstRowCols.length; i++) {
+                headers.put(i, i.toString());
             }
         }
 
+        //Percorre linhas
+        for (String line : lines) {
+            if (!line.startsWith("#")) {
+                //Pega colunas da linha
+                String[] cols = line.split(colBreaker);
+
+                //tryCatch para evitar parar tudo caso alguma linha tenha mais colunas que a primeira
+                try {
+                    Map<String, String> map = new LinkedHashMap<>();
+
+                    //Para cada header coloca no mapa
+                    headers.forEach((i, name) -> {
+                        map.put(name, cols[i]);
+                    });
+
+                    //coloca mapa na lista
+                    rows.add(map);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return rows;
     }
 }
